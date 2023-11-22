218651251a11c8805ac44feccae2a09ad95b4484
JulianAballay <juliancabj1210@gmail.com>
2023-11-22 01:26:46 -0300

    Agrego mejora logica en el ejercicio

--- js/ejercicio2POO.js
+++ js/ejercicio2POO.js
@@ -13,7 +13,9 @@ const cuenta = {
       document.write(
         '<p class="text-warning text-center fs-1">Usted no posee fondos suficientes!! </p>'
       );
+      return this.saldo;
     }
+    
   },
   informar: function (estado) {
     document.write(`<p class="text-center text-success fs-1"> Saldo actual de la cuenta: ${estado}</p>`);
@@ -37,6 +39,7 @@ if (Number.isInteger(agregarSaldo)) {
 let extraerSaldo = parseInt(prompt("Extraer saldo"));
 if (Number.isInteger(extraerSaldo)) {
   saldoCuenta = cuenta.extraer(extraerSaldo);
+  document.write(`<p class="text-center text-success fs-1"> Usted extrajo: $${extraerSaldo} de su cuenta</p>`);
   cuenta.informar(saldoCuenta);
 } else {
   alert("El valor ingresado no es válido!");
